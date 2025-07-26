ce9ade99437d95f12e4307494e0a689ea97acc8f
krishnajithkicha <krishnajithkicha@gmail.com>
2025-07-26 09:28:29 +0530

    commit

--- traffic_system/traffic_system/settings.py
+++ traffic_system/traffic_system/settings.py
@@ -32,6 +32,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # for collectstatic
 # Application definition
 
 INSTALLED_APPS = [
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -129,3 +130,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
